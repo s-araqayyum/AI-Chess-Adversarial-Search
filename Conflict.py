2d71e273f27642772eb13e2a2fac45091c96bd04
Sara <114652654+s-araqayyum@users.noreply.github.com>
2023-05-01 02:09:29 +0500

    Delete Conflict.py

--- Conflict.py
+++
@@ -1,5 +0,0 @@
-class Conflict:
-    def __init__(self, course_1, course_2, number_of_conflicting_students):
-        self.course_1 = course_1
-        self.course_2 = course_2
-        self.number_of_conflicting_students = number_of_conflicting_students
